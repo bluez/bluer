b22729cf5e7ac22005bb94e39827bcef53068f0b
Attila Dusnoki <dati91@gmail.com>
2016-02-22 17:25:39 +0100

    Refactor adapter and device attributes

--- src/bluetooth_adapter.rs
+++ src/bluetooth_adapter.rs
@@ -2,12 +2,10 @@ use dbus::{Connection, BusType, Props};
 use bluetooth_utils;
 use bluetooth_device::BluetoothDevice;
 
-#[allow(dead_code)]
 static ADAPTER_INTERFACE: &'static str = "org.bluez.Adapter1";
 static SERVICE_NAME: &'static str = "org.bluez";
-static DEVICE_INTERFACE: &'static str = "org.bluez.Device1";
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct BluetoothAdapter {
     object_path: String,
 }
@@ -35,15 +33,32 @@ impl BluetoothAdapter {
         if devices.is_empty() {
             return Err(String::from("No device found."))
         }
+        Ok(BluetoothDevice::create_device(devices[0].clone()))
+    }
 
+    pub fn get_devices(&self) -> Vec<String> {
+        bluetooth_utils::list_devices(&self.object_path)
+    }
+
+    pub fn get_object_path(&self) -> String {
+        self.object_path.clone()
+    }
+
+    pub fn get_address(&self) -> String {
         let c = Connection::get_private(BusType::System).unwrap();
-        let d = Props::new(&c, SERVICE_NAME, &devices[0], DEVICE_INTERFACE, 10000);
-        Ok(BluetoothDevice::new(String::from(d.get("Address").unwrap().inner::<&str>().unwrap()),
-                                String::from(d.get("Name").unwrap().inner::<&str>().unwrap()),
-                                d.get("Class").unwrap().inner::<u32>().unwrap(),
-                                0u32,
-                                0u32,
-                                0u32,
-                                None))
+        let d = Props::new(&c, SERVICE_NAME, &self.object_path, ADAPTER_INTERFACE, 10000);
+        String::from(d.get("Address").unwrap().inner::<&str>().unwrap())
+    }
+
+    pub fn get_name(&self) -> String {
+        let c = Connection::get_private(BusType::System).unwrap();
+        let d = Props::new(&c, SERVICE_NAME, &self.object_path, ADAPTER_INTERFACE, 10000);
+        String::from(d.get("Name").unwrap().inner::<&str>().unwrap())
+    }
+
+    pub fn get_class(&self) -> u32 {
+        let c = Connection::get_private(BusType::System).unwrap();
+        let d = Props::new(&c, SERVICE_NAME, &self.object_path, ADAPTER_INTERFACE, 10000);
+        d.get("Class").unwrap().inner::<u32>().unwrap()
     }
 }
--- src/bluetooth_device.rs
+++ src/bluetooth_device.rs
@@ -1,55 +1,72 @@
-#[derive(Debug)]
+use dbus::{Connection, BusType, Props};
+
+static SERVICE_NAME: &'static str = "org.bluez";
+static DEVICE_INTERFACE: &'static str = "org.bluez.Device1";
+
+#[derive(Clone, Debug)]
 pub struct BluetoothDevice {
-    address: String,
-    name: String,
-    class: u32,
-    vendor_id: u32,
-    product_id: u32,
-    product_version: u32,
-    uuids: Option<Vec<String>>,
+    object_path: String,
 }
 
 impl BluetoothDevice {
-    pub fn new(address: String,
-           name: String,
-           class: u32,
-           vendor_id: u32,
-           product_id: u32,
-           product_version: u32,
-           uuids: Option<Vec<String>>)
+    fn new(object_path: String)
            -> BluetoothDevice {
         BluetoothDevice {
-            address: address,
-            name: name,
-            class: class,
-            vendor_id: vendor_id,
-            product_id: product_id,
-            product_version: product_version,
-            uuids: uuids
+            object_path: object_path
         }
     }
 
-    pub fn address(&self) -> String {
-      self.address.clone()
+    pub fn create_device(object_path: String) -> BluetoothDevice {
+        BluetoothDevice::new(object_path)
     }
 
-    pub fn name(&self) -> String {
-      self.name.clone()
+    pub fn get_object_path(&self) -> String {
+        self.object_path.clone()
     }
 
-    pub fn class(&self) -> u32 {
-      self.class
+    pub fn get_address(&self) -> String {
+        let c = Connection::get_private(BusType::System).unwrap();
+        let d = Props::new(&c, SERVICE_NAME, &self.object_path, DEVICE_INTERFACE, 10000);
+        String::from(d.get("Address").unwrap().inner::<&str>().unwrap())
     }
 
-    pub fn vendor_id(&self) -> u32 {
-      self.vendor_id
+    pub fn get_name(&self) -> String {
+        let c = Connection::get_private(BusType::System).unwrap();
+        let d = Props::new(&c, SERVICE_NAME, &self.object_path, DEVICE_INTERFACE, 10000);
+        String::from(d.get("Name").unwrap().inner::<&str>().unwrap())
     }
 
-    pub fn product_id(&self) -> u32 {
-      self.product_id
+    pub fn get_class(&self) -> u32 {
+        let c = Connection::get_private(BusType::System).unwrap();
+        let d = Props::new(&c, SERVICE_NAME, &self.object_path, DEVICE_INTERFACE, 10000);
+        d.get("Class").unwrap().inner::<u32>().unwrap()
     }
 
-    pub fn product_version(&self) -> u32 {
-      self.product_version
+    pub fn get_vendor_id(&self) -> u32 {
+        let vendor_id = 0u32;
+        let modalias = self.get_modalias();
+        //TODO get vendor_id
+        vendor_id
     }
+
+    pub fn get_product_id(&self) -> u32 {
+        let product_id = 0u32;
+        let modalias = self.get_modalias();
+        //TODO get product_id
+        product_id
+    }
+
+    pub fn get_product_version(&self) -> u32 {
+        let product_id = 0u32;
+        let modalias = self.get_modalias();
+        //TODO get product_id
+        product_id
+    }
+
+    fn get_modalias(&self) -> String {
+        let c = Connection::get_private(BusType::System).unwrap();
+        let d = Props::new(&c, SERVICE_NAME, &self.object_path, DEVICE_INTERFACE, 10000);
+        String::from(d.get("Modalias").unwrap().inner::<&str>().unwrap())
+    }
+
 }
