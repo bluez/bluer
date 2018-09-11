6fbfc7228ebdc136ac580b34a137652c6f03c933
Gal Ben-Haim <gbenhaim@augury.com>
2018-09-11 22:42:31 +0300

    added path filter to BluetoothSession
    BluetoothDiscoverySession now uses a connection from BluetoothSession
    
    filter only signals

--- src/bluetooth_discovery_session.rs
+++ src/bluetooth_discovery_session.rs
@@ -1,34 +1,46 @@
+use bluetooth_session::BluetoothSession;
 use dbus::{BusType, Connection, Message, MessageItem};
 use std::error::Error;
 
 static ADAPTER_INTERFACE: &'static str = "org.bluez.Adapter1";
 static SERVICE_NAME: &'static str = "org.bluez";
 
-pub struct BluetoothDiscoverySession {
+pub struct BluetoothDiscoverySession<'a> {
     adapter: String,
-    connection: Connection,
+    session: &'a BluetoothSession,
 }
 
-impl BluetoothDiscoverySession {
-    pub fn create_session(adapter: String) -> Result<BluetoothDiscoverySession, Box<Error>> {
-        let c = try!(Connection::get_private(BusType::System));
-        Ok(BluetoothDiscoverySession::new(adapter, c))
+impl<'a> BluetoothDiscoverySession<'a> {
+    pub fn create_session(
+        session: &'a BluetoothSession,
+        adapter: String,
+    ) -> Result<BluetoothDiscoverySession, Box<Error>> {
+        Ok(BluetoothDiscoverySession::new(session, adapter))
     }
 
-    fn new(adapter: String, connection: Connection) -> BluetoothDiscoverySession {
+    fn new(session: &'a BluetoothSession, adapter: String) -> BluetoothDiscoverySession<'a> {
         BluetoothDiscoverySession {
             adapter: adapter,
-            connection: connection,
+            session: session,
         }
     }
 
     fn call_method(&self, method: &str, param: Option<[MessageItem; 1]>) -> Result<(), Box<Error>> {
-        let mut m = try!(Message::new_method_call(SERVICE_NAME, &self.adapter, ADAPTER_INTERFACE, method));
+        let mut m = try!(Message::new_method_call(
+            SERVICE_NAME,
+            &self.adapter,
+            ADAPTER_INTERFACE,
+            method
+        ));
         match param {
             Some(p) => m.append_items(&p),
             None => (),
         };
-        try!(self.connection.send_with_reply_and_block(m, 1000));
+        try!(
+            self.session
+                .get_connection()
+                .send_with_reply_and_block(m, 1000)
+        );
         Ok(())
     }
 
--- src/bluetooth_session.rs
+++ src/bluetooth_session.rs
@@ -1,35 +1,117 @@
-use dbus::{arg::cast, arg::RefArg, arg::TypeMismatchError, arg::Variant, BusType, Connection};
+use dbus::{
+    arg::cast, arg::RefArg, arg::TypeMismatchError, arg::Variant, BusType, ConnMsgs, Connection,
+    Message,
+};
 use std::collections::HashMap;
 use std::error::Error;
 
-use bluetooth_adapter::BluetoothAdapter;
-use bluetooth_device::BluetoothDevice;
-
-use std::sync::mpsc;
-
-static BLUEZ_MATCH: &'static str = "sender='org.bluez'";
+static BLUEZ_MATCH: &'static str = "type='signal',sender='org.bluez'";
 
 pub struct BluetoothSession {
     connection: Connection,
-    powered_callback: Option<Box<Fn(BluetoothAdapter, &bool)>>,
-    discovering_callback: Option<Box<Fn(BluetoothAdapter, &bool)>>,
-    connected_callback: Option<Box<Fn(BluetoothDevice, &bool)>>,
-    services_resolved_callback: Option<Box<Fn(BluetoothDevice, &bool)>>,
+}
+
+#[derive(Clone)]
+pub enum BluetoothEvent {
+    Powered {
+        object_path: String,
+        powered: bool,
+    },
+    Discovering {
+        object_path: String,
+        discovering: bool,
+    },
+    Connected {
+        object_path: String,
+        connected: bool,
+    },
+    ServicesResolved {
+        object_path: String,
+        services_resolved: bool,
+    },
+    None,
+}
+
+impl BluetoothEvent {
+    pub fn from(conn_msg: Message) -> Option<BluetoothEvent> {
+        let result: Result<
+            (&str, HashMap<String, Variant<Box<RefArg>>>),
+            TypeMismatchError,
+        > = conn_msg.read2();
+
+        match result {
+            Ok((_, properties)) => {
+                let object_path = conn_msg.path().unwrap().to_string();
+
+                if let Some(value) = properties.get("Powered") {
+                    if let Some(powered) = cast::<bool>(&value.0) {
+                        let event = BluetoothEvent::Powered {
+                            object_path: object_path.clone(),
+                            powered: *powered,
+                        };
+
+                        return Some(event);
+                    }
+                }
+
+                if let Some(value) = properties.get("Discovering") {
+                    if let Some(discovering) = cast::<bool>(&value.0) {
+                        let event = BluetoothEvent::Discovering {
+                            object_path: object_path.clone(),
+                            discovering: *discovering,
+                        };
+
+                        return Some(event);
+                    }
+                }
+
+                if let Some(value) = properties.get("Connected") {
+                    if let Some(connected) = cast::<bool>(&value.0) {
+                        let event = BluetoothEvent::Connected {
+                            object_path: object_path.clone(),
+                            connected: *connected,
+                        };
+
+                        return Some(event);
+                    }
+                }
+
+                if let Some(value) = properties.get("ServicesResolved") {
+                    if let Some(services_resolved) = cast::<bool>(&value.0) {
+                        let event = BluetoothEvent::ServicesResolved {
+                            object_path: object_path.clone(),
+                            services_resolved: *services_resolved,
+                        };
+
+                        return Some(event);
+                    }
+                }
+
+                Some(BluetoothEvent::None)
+            }
+            Err(err) => None,
+        }
+    }
 }
 
 impl BluetoothSession {
-    pub fn create_session() -> Result<BluetoothSession, Box<Error>> {
+    pub fn create_session(path: Option<&str>) -> Result<BluetoothSession, Box<Error>> {
+        let mut rule = {
+            if let Some(path) = path {
+                format!("{},path='{}'", BLUEZ_MATCH, path)
+            } else {
+                String::from(BLUEZ_MATCH)
+            }
+        };
+
         let c = try!(Connection::get_private(BusType::System));
+        c.add_match(rule.as_str())?;
         Ok(BluetoothSession::new(c))
     }
 
     fn new(connection: Connection) -> BluetoothSession {
         BluetoothSession {
             connection: connection,
-            powered_callback: None,
-            discovering_callback: None,
-            connected_callback: None,
-            services_resolved_callback: None,
         }
     }
 
@@ -37,84 +119,9 @@ impl BluetoothSession {
         &self.connection
     }
 
-    pub fn set_powered_callback(&mut self, callback: Box<Fn(BluetoothAdapter, &bool)>) {
-        self.powered_callback = Some(callback);
-    }
-
-    pub fn set_discovering_callback(&mut self, callback: Box<Fn(BluetoothAdapter, &bool)>) {
-        self.discovering_callback = Some(callback);
-    }
-
-    pub fn set_connected_callback(&mut self, callback: Box<Fn(BluetoothDevice, &bool)>) {
-        self.connected_callback = Some(callback);
-    }
-
-    pub fn set_services_resolved_callback(&mut self, callback: Box<Fn(BluetoothDevice, &bool)>) {
-        self.services_resolved_callback = Some(callback);
-    }
-
-    pub fn listen(&mut self, terminate: mpsc::Receiver<bool>) -> Result<(), Box<Error>> {
-        self.connection.add_match(BLUEZ_MATCH)?;
-        loop {
-            for conn_msg in self.connection.incoming(1000) {
-                let result: Result<
-                    (&str, HashMap<String, Variant<Box<RefArg>>>),
-                    TypeMismatchError,
-                > = conn_msg.read2();
-
-                match result {
-                    Ok((_, properties)) => {
-                        let object_path = conn_msg.path().unwrap().to_string();
-
-                        if let Some(value) = properties.get("Powered") {
-                            if let Some(powered) = cast::<bool>(&value.0) {
-                                if let Some(ref callback) = self.powered_callback {
-                                    callback(
-                                        BluetoothAdapter::create_adapter(self, object_path.clone())
-                                            .unwrap(),
-                                        powered,
-                                    );
-                                }
-                            }
-                        }
-
-                        if let Some(value) = properties.get("Discovering") {
-                            if let Some(discovering) = cast::<bool>(&value.0) {
-                                if let Some(ref callback) = self.discovering_callback {
-                                    callback(
-                                        BluetoothAdapter::create_adapter(self, object_path.clone())
-                                            .unwrap(),
-                                        discovering,
-                                    );
-                                }
-                            }
-                        }
-
-                        if let Some(value) = properties.get("Connected") {
-                            if let Some(c) = cast::<bool>(&value.0) {
-                                if let Some(ref callback) = self.connected_callback {
-                                    callback(BluetoothDevice::new(self, object_path.clone()), c);
-                                }
-                            }
-                        }
-
-                        if let Some(value) = properties.get("ServicesResolved") {
-                            if let Some(s) = cast::<bool>(&value.0) {
-                                println!("{} services_resolved={}", conn_msg.path().unwrap(), s);
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            }
-
-            let t = terminate.try_recv();
-            match t {
-                Ok(true) => {
-                    return Ok(());
-                }
-                _ => {}
-            }
-        }
+    pub fn incoming(&self, timeout_ms: u32) -> ConnMsgs<&Connection> {
+        self.connection.incoming(timeout_ms)
     }
 }
+
+unsafe impl Send for BluetoothEvent {}
