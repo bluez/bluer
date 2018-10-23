91da8ebeccf3a5efbd42e9db386e7c47e6cd319a
Gal Ben-Haim <gbenhaim@augury.com>
2018-10-23 08:59:28 +0300

    implemented BluetoothDiscoverySession::set_discovery_filter()

--- src/bluetooth_discovery_session.rs
+++ src/bluetooth_discovery_session.rs
@@ -1,5 +1,5 @@
 use bluetooth_session::BluetoothSession;
-use dbus::{Message, MessageItem};
+use dbus::{Message, MessageItem, MessageItemArray, Signature};
 use std::error::Error;
 
 static ADAPTER_INTERFACE: &'static str = "org.bluez.Adapter1";
@@ -50,5 +50,48 @@ impl<'a> BluetoothDiscoverySession<'a> {
 
     pub fn stop_discovery(&self) -> Result<(), Box<Error>> {
         self.call_method("StopDiscovery", None)
+    }
+
+    pub fn set_discovery_filter(
+        &self,
+        uuids: Vec<String>,
+        rssi: Option<i16>,
+        pathloss: Option<u16>,
+    ) -> Result<(), Box<Error>> {
+        let uuids = {
+            let mut res: Vec<MessageItem> = Vec::new();
+            for u in uuids {
+                res.push(u.into());
+            }
+            res
+        };
+
+        let mut m = vec![MessageItem::DictEntry(
+            Box::new("UUIDs".into()),
+            Box::new(MessageItem::Variant(Box::new(
+                MessageItem::new_array(uuids).unwrap(),
+            ))),
+        )];
+
+        if let Some(rssi) = rssi {
+            m.push(MessageItem::DictEntry(
+                Box::new("RSSI".into()),
+                Box::new(MessageItem::Variant(Box::new(rssi.into()))),
+            ))
+        }
+
+        if let Some(pathloss) = pathloss {
+            m.push(MessageItem::DictEntry(
+                Box::new("Pathloss".into()),
+                Box::new(MessageItem::Variant(Box::new(pathloss.into()))),
+            ))
+        }
+
+        self.call_method(
+            "SetDiscoveryFilter",
+            Some([MessageItem::Array(
+                MessageItemArray::new(m, Signature::from("a{sv}")).unwrap(),
+            )]),
+        )
     }
 }
