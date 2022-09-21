2a77d8baf75feb9ef9fb8eb15d3b77376bdd350d
otaviojr <otavio.ribeiro@gmail.com>
2022-09-21 11:06:34 -0300

    monitor

--- bluer/src/adapter.rs
+++ bluer/src/adapter.rs
@@ -27,7 +27,7 @@ use crate::{
     device::Device,
     gatt, Address, AddressType, Error, ErrorKind, Event, InternalErrorKind, Modalias, Result, SessionInner,
     SingleSessionToken, SERVICE_NAME, TIMEOUT,
-    monitor::{Monitor, RegisteredMonitor},
+    monitor::{Monitor, RegisteredMonitor, MonitorHandle},
 };
 
 pub(crate) const INTERFACE: &str = "org.bluez.Adapter1";
