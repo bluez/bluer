e9719041c9fbe99cda01d6a7766e86cd8ac8bb2d
otaviojr <otavio.ribeiro@gmail.com>
2022-09-21 14:50:33 -0300

    monitor

--- bluer/src/monitor.rs
+++ bluer/src/monitor.rs
@@ -231,8 +231,7 @@ impl RegisteredMonitor {
             ib.property("RSSIHighTimeout").get(|_,_monitor| Ok(2 as i16));
             ib.property("RSSISamplingPeriod").get(|_,_monitor| Ok(0 as i16));
             ib.property("Patterns").get({|_,_monitor| 
-                let pattern: [u8; 9 as usize] = [0, 1, 127, 0, 127, 0, 5, 0x99,'_'.into()];
-                Ok(pattern)
+                Ok(vec![0, 1, 127, 0, 127, 0, 5, 0x99,'_'.into()] as &[u8])
             });
         })
     }
