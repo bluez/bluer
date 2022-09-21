c19b98b354c67eb2c80e82f044125c4504d99d55
otaviojr <otavio.ribeiro@gmail.com>
2022-09-21 12:19:00 -0300

    monitor

--- bluer/src/monitor.rs
+++ bluer/src/monitor.rs
@@ -108,9 +108,9 @@ pub struct Monitor {
 
 impl Monitor {
 
-    pub(crate) fn new(inner: Arc<SessionInner>, callbacks: MonitorCallbacks) -> Self {
+    pub(crate) fn new(inner: Arc<SessionInner>, callbacks: MonitorCallbacks) -> Result<Self> {
         let name = dbus::Path::new(format!("{}/{}", MANAGER_PATH,Uuid::new_v4().as_simple())).unwrap();
-        Self { inner: inner, callbacks: callbacks, dbus_path: name }
+        Ok(Self { inner: inner, callbacks: callbacks, dbus_path: name })
     }
 
     fn proxy(&self) -> Proxy<'_, &SyncConnection> {
