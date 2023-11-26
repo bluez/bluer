f5c0f6e5532292ec9c2da0b240a156c914d0ccc3
Sebastian Urban <surban@surban.net>
2023-11-26 18:36:13 +0100

    monitor example: print instead of log

--- bluer/examples/le_passive_scan.rs
+++ bluer/examples/le_passive_scan.rs
@@ -6,10 +6,12 @@ use futures::StreamExt;
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
     env_logger::init();
+
     let session = bluer::Session::new().await?;
     let adapter = session.default_adapter().await?;
-    let mm = adapter.monitor().await?;
     adapter.set_powered(true).await?;
+
+    let mm = adapter.monitor().await?;
     let mut monitor_handle = mm
         .register(Monitor {
             monitor_type: bluer::monitor::Type::OrPatterns,
@@ -25,12 +27,12 @@ async fn main() -> bluer::Result<()> {
 
     while let Some(mevt) = &monitor_handle.next().await {
         if let MonitorEvent::DeviceFound(devid) = mevt {
-            log::info!("Discovered device {:?}", devid);
+            println!("Discovered device {:?}", devid);
             let dev = adapter.device(devid.device)?;
             tokio::spawn(async move {
                 let mut events = dev.events().await.unwrap();
                 while let Some(ev) = events.next().await {
-                    log::info!("On device {:?}, received event {:?}", dev, ev);
+                    println!("On device {:?}, received event {:?}", dev, ev);
                 }
             });
         }
